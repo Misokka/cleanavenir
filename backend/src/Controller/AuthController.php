f9c30ca45c365bf9afe05a0eb2b646af039d885b
Jonathan Assi <assijonathan2@gmail.com>
2025-09-15 19:07:00 +0200

    ajout du système d'auth et de session côté back

--- backend/src/Controller/AuthController.php
+++ backend/src/Controller/AuthController.php
@@ -5,11 +5,14 @@ namespace App\Controller;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 #[Route('/api/auth', methods: ['POST'])]
 final class AuthController extends AbstractController
@@ -17,7 +20,8 @@ final class AuthController extends AbstractController
     #[Route('/register', name: 'app_register')]
     public function store(
         Request $request, 
-        SerializerInterface $serializer, 
+        SerializerInterface $serializer,
+        UserPasswordHasherInterface $passwordHasher,
         EntityManagerInterface $entityManager,
         ValidatorInterface $validator
     ): JsonResponse
@@ -36,11 +40,36 @@ final class AuthController extends AbstractController
             return $this->json(['errors' => $errorsArr]);
         }
 
+        $hashedPassword = $passwordHasher->hashPassword($newUser, $newUser->getPassword());
+        $newUser->setPassword($hashedPassword);
         $entityManager->persist($newUser);
         $entityManager->flush();
 
         return $this->json([
-            'message' => 'utilisateur créé avec succès.'
+            'message' => 'utilisateur créé avec succès.',
+            'user' => $newUser,
+        ]);
+    }
+
+    #[Route('/login', name: 'app_login')]
+    public function login(#[CurrentUser] ?User $user){
+        if(null === $user) {
+            return $this->json([
+                'message' => 'Missing credentials'
+            ], 401);
+        }
+
+        return $this->json([
+            'message' => 'Connexion réussie'
+        ]);
+    }
+
+    #[Route('/logout', name: 'app_logout')]
+    public function logout(Security $security){
+        $security->logout(false);
+
+        return $this->json([
+            'message' => 'Déconnexion réussie.'
         ]);
     }
 }
